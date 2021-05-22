3070703956389f09176412e355c79983bcb86761
mr-shrayan <theemptyunexplored@gmail.com>
2021-05-22 20:22:09 +0530

    Maincode Edit

--- MainCode.py
+++ MainCode.py
@@ -36,19 +36,19 @@ label_2.place(x=50,y=180)
 
 label_3=Label(root1,text="2. After Reaching the board Select Starting and Ending Node by Left Click of the Mouse.\n",font=(10))
 label_3.place(x=50,y=220)
-label_8=Label(root1,text="      You can unselect them by clicking on the same Node using Right Click.\n",font=(10))
+label_8=Label(root1,text="      You can unselect them by clicking on the same Node using Right Click Or Press with 2 fingers for mac users\n",font=(10))
 label_8.place(x=50,y=260)
 
 label_4=Label(root1,text="3. Once selecting those 2 points one can select any Node as an obstacle apart from Start and End Node.\n",font=(10))
 label_4.place(x=50,y=300)
 label_5=Label(root1,text="4. To Start The Algorithm, Press Space Bar!!!!.\n",font=(10))
 label_5.place(x=50,y=340)
-label_6=Label(root1,text="5. One Can Rerun the same Algorithm using the same Space Bar(Only after exection of the Algorithm).\n",font=(10))
+label_6=Label(root1,text="5. One Can Rerun the same Algorithm using the same Space Bar(Only after execution of the Algorithm).\n",font=(10))
 label_6.place(x=50,y=380)
 label_7=Label(root1,text="6. Press Ctrl+C to clear all the obstacle this will clear the entire Board including Start and End Node.\n",font=(10))
 label_7.place(x=50,y=420)
 
-Button(root1, text='Skip Guide...):' ,font=(20),width=20,bg="black",fg='white',command=get).place(x=380,y=580)
+Button(root1, text='Skip Guide):' ,font=(20),width=20,bg="black",fg='white',command=get).place(x=380,y=580)
 
 root1.mainloop()
 
@@ -60,7 +60,7 @@ root = Tk()
 
 root.geometry("500x500")
 
-root.title('Alley Vizualizer')
+root.title('Beeline Perceiver')
 
 entry_4 = StringVar()
 entry_1 = StringVar()
@@ -575,7 +575,7 @@ def algorithm(Algorithm,draw, grid, start, end,diag):
 				current.make_closed()
 		
 
-	print("Algorithm Failed!!")
+	print("Algorithm Failed!! Execution Stopped!!")
 
 	return False
 
@@ -722,12 +722,6 @@ def main(Algorithm,ob,diag):
 					grid = make_grid(cols,ROWS, width,height,diag)
 
 	pygame.quit()
-
-
-
-
-
-		
 
 
 root.mainloop()
